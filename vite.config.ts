5246599b6e2f58d62467017033c462600fb775ce
yanergy <56261297+yanergy@users.noreply.github.com>
2026-08-26 22:22:03 +0200

    Added Shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import electron from 'vite-plugin-electron/simple'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +12,7 @@ export default defineConfig({
     plugins: [
         vue(),
         vueDevTools(),
+        tailwindcss(),
         electron({
             main: { entry: 'electron/main.cjs' },
         }),
